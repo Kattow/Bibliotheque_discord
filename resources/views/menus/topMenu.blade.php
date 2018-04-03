5491d02e0678a20d0af6d0624f6878c8731bbb9f
Salvatore8301 <trappeur83var@gmail.com>
2018-04-03 17:43:33 +0000

    menu
    changer le menu deroulant, au lieu de mettre des div, des listes

--- resources/views/menus/topMenu.blade.php
+++ resources/views/menus/topMenu.blade.php
@@ -8,88 +8,14 @@
     
     <header>
 
-        //premiere case
-        <div class="">
-
-            //zone principale (visible tout le temps)
-            <h1>Accueil</h1>
-
-            //zone hidden 
-            <div class="secondaire">
-                
-            </div>
-
-            <div class="secondaire">
-            
-            </div>
-
-            <div class="secondaire">
-            
-            </div>
-
-        </div>
+        <nav>
+                <ul>
+                    <li> <a href=""></a> </li>
+                    <li> <a href=""></a> </li>
+                    <li> <a href=""></a> </li>                   
+                </ul>
         
-        //deuxieme case
-        <div class="">
-            
-            //zone principale
-            <h1>Oeuvre</h1>
-
-            //zone hidden
-            <div class="secondaire">
-            
-            </div>
-
-            <div class="secondaire">
-            
-            </div>
-
-            <div class="secondaire">
-            
-            </div>
-
-            <div class="secondaire">
-            
-            </div>
-
-            <div class="secondaire">
-            
-            </div>
-
-
-        </div>
-
-        //troisieme case
-        <div class="">
-
-            //zone principale
-            <h1>Forum</h1>
-
-            //zone hidden
-            <div class="secondaire">
-            
-            </div>
-
-            <div class="secondaire">
-            
-            </div>
-
-        </div>
-
-        //quatrieme case
-        <div class="">
-
-            //zone principale 
-            <h1>Recherche</h1>
-
-            //zone hidden
-            <div class="secondaire">
-            
-            </div>
-        
-        
-        </div>
-
+        </nav>
 
     </header>
 
