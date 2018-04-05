f6920de0d007a5b3d135a98e4e8b080147833f92
Salvatore8301 <trappeur83var@gmail.com>
2018-04-05 15:28:02 +0000

    probleme css
    probleme css

--- resources/views/menus/topMenu.blade.php
+++ resources/views/menus/topMenu.blade.php
@@ -1,28 +1,29 @@
 
 
-    <nav>
+    <nav class="topmenu-nav">
         <ul>                
-                <li> 
-                    <ul>
-                        <li><a href="http://biblio/accueil">Accueil</a> </li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
+            <li> 
+                <ul>
+                    <li><a href="http://biblio/accueil" class="lien">Accueil</a> </li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </li>
                 
-                <li> 
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        </ul>
-                        </li>
-                        <li> 
-                            <ul>
-                                <li> <a href="http://biblio/contact">Contact</a> </li>
-                            </ul>
-                        </li>                   
-            </ul>
+            <li> 
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </li>
+    
+            <li> 
+                <ul>
+                    <li> <a href="http://biblio/contact" class="lien">Contact</a> </li>
+                </ul>
+            </li>                   
+        </ul>
     </nav>
         
