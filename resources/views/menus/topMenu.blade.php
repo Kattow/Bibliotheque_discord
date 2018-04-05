e2ef2f165d9601f1d984ad505fc5c73cfece18b5
Kattow <37405746+Kattow@users.noreply.github.com>
2018-04-05 17:11:15 +0200

    indent

--- resources/views/menus/topMenu.blade.php
+++ resources/views/menus/topMenu.blade.php
@@ -1,28 +1,25 @@
 
-
-    <nav>
-        <ul>                
+<nav>
+    <ul>
+        <li> 
+            <ul>
+                <li><a href="http://biblio/accueil">Accueil</a> </li>
+                <li></li>
+                <li></li>
+            </ul>
                 <li> 
                     <ul>
-                        <li><a href="http://biblio/accueil">Accueil</a> </li>
-                        <li></li>
-                        <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
                     </ul>
                 </li>
-                
                 <li> 
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        </ul>
-                        </li>
-                        <li> 
-                            <ul>
-                                <li> <a href="http://biblio/contact">Contact</a> </li>
-                            </ul>
-                        </li>                   
-            </ul>
-    </nav>
-        
+                        <li> <a href="http://biblio/contact">Contact</a> </li>
+                    </ul>
+                </li>
+        </li>                   
+    </ul>
+</nav>
