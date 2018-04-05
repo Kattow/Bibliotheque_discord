aba18c3583ca48dd60e3a24f8d3ef680a7c9fd1b
Salvatore8301 <trappeur83var@gmail.com>
2018-04-05 18:41:35 +0000

    menu
    ajout des sous liens

--- resources/views/menus/topMenu.blade.php
+++ resources/views/menus/topMenu.blade.php
@@ -4,28 +4,46 @@
         <ul class="ul-menu">                
             
             
-            <ul class="ul-menu-accueil">
-                <li> <a href="http://biblio/accueil" class="lien">Accueil</a> </li>
-                <li></li>
-                <li></li>
+            <ul class="ul-menu-accueil">    
+                    <li> <a href="http://biblio/accueil" class="lien">Accueil</a> 
+                        <ul class="sousmenu">
+                            <li> <a href="#" class="lien">Annonces</a> </li>
+                            <li> <a href="#" class="lien">reseaux sociaux</a> </li>
+                            <li> <a href="#" class="lien">evenements</a> </li>
+                        </ul>
+                    </li>
             </ul>
 
                  
             <ul class="ul-menu-oeuvres">
-                <li> <a href="#" class="lien">Oeuvres</a> </li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li> <a href="#" class="lien">Oeuvres</a> 
+                    <ul class="sousmenu">    
+                        <li> <a href="#" class="lien">Ecrivain du mois</a> </li>
+                        <li> <a href="#" class="lien">Illustrations</a> </li>
+                        <li> <a href="#" class="lien">Liste des oeuvres</a> </li>
+                        <li> <a href="#" class="lien">Meilleures oeuvres</a> </li>
+                        <li> <a href="#" class="lien">Random book generator</a> </li>
+                    </ul>
+                </li>
             </ul>
     
              
             <ul class="ul-menu-forum">
-                <li> <a href="http://biblio/contact" class="lien">Contact</a> </li>
-                <li></li>
+                <li> <a href="http://biblio/contact" class="lien">Contact</a> 
+                    <ul class="sousmenu">
+                        <li> <a href="#" class="lien">Forum</a> </li>
+                        <li> <a href="#" class="lien">F.A.Q</a> </li>
+                    </ul>    
+                </li>
             </ul>
             
             <ul class="ul-menu-recherches">
-                <li> <a href="#" class="lien">Recherche</a> </li>
+                <li> <a href="#" class="lien">Recherche</a> 
+                    <ul class="sousmenu">
+                        <li> <a href="#" class="lien">recherches avancées</a> </li>
+                    </ul>
+                </li>
+                
             </ul>               
         </ul>
     </nav>
