62765465e1bbdbee6f09f53115cad7f032cd89c4
Kattow <37405746+Kattow@users.noreply.github.com>
2018-04-05 21:18:05 +0200

    yield

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Accueil</title>
-    <link rel="stylesheet" href="css/topmenu.css" style="type/css">
-</head>
-<body>
 @extends('menus/topMenu')
-    </body>
-</html>
+
+@section('content')
+    <h1>Laccueil du site</h1>
+@endsection
