086bf4532fc1ac08ba5054d442166d627e72a1e8
Salvatore8301 <trappeur83var@gmail.com>
2018-04-14 11:29:11 +0000

    Faq csss
    ajout du css pour le formulaire d'envoye de la faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="content-formulaire-faq">
 	<div class="formulaire-faq">
-		<h4>Ajouter un question :</h4>
+		<h4>Ajouter une question :</h4>
 
 <!--
 Les liens et les envoye a peut être changer
@@ -16,14 +16,22 @@ Comme tu veux Kattow comme c'est toi qui gere cette partie ^^
 Tu pourras ajouter ceux que tu veux, la j'ai mis ce qui me venait a l'esprit
 comme je sais pas quel themes on va mettre pour le moment
 -->
-				{!! Form::select('objet', ['Futur', 'Actualité', 'Events', 'Lectures', 'Illustrations']) !!}
-			</div>
-			<div {!! $errors->has('question') ? 'has-errors' : '' !!}>
-					{!! Form::label('question-faq', 'Question posée :') !!}
-					{!! Form::textarea('question-faq', null, ['class' => 'textarea-formulaire-faq']) !!}
-			</div>
-			<div class="">
+				{!! Form::select('objet', ['Futur', 'Actualité', 'Events', 'Lectures', 'Illustrations'], ['class' => 'radio-objet-faq']) !!}
 
+			</div>
+			<br>
+			<br>
+			<div {!! $errors->has('question') ? 'has-errors' : '' !!}>
+					{!! Form::label('question-faq', 'Question posée :', ['class' => 'label-question-faq']) !!}
+					<br>
+					{!! Form::textarea('question-faq', null, ['class' => 'textarea-formulaire-question-faq']) !!}
+			</div>
+			<br>
+			<br>
+			<div {{!! $errors->has('reponse') ? 'has-errors' : '' !!}}>
+					{!! Form::label('reponse-faq', 'Reponse :', ['class' => 'label-reponse-faq']) !!}
+					<br>
+					{!! Form::textarea('reponse-faq', null, ['class' => 'textarea-formulaire-reponse-faq']) !!}
 			</div>
 			{!! Form::submit('Ajouter la question', ['class' => 'bouton-faq-envoyer']) !!}
 		{!! Form::close() !!}
@@ -42,7 +50,7 @@ comme je sais pas quel themes on va mettre pour le moment
 			<h3>La question</h3>
 
 		</div>
-		<div class="reposne-faq">
+		<div class="reponse-faq">
 			<p>La réponse</p>
 		</div>
 	</div>
