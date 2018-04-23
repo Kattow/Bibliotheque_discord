fa35be80be909a4cfdb9846b49d4821e3f6269e1
Salvatore8301 <trappeur83var@gmail.com>
2018-04-23 08:36:44 +0000

    De la merde

--- resources/views/maintenance/bannièremaintenance.blade.php
+++ resources/views/maintenance/bannièremaintenance.blade.php
@@ -1,15 +1,1 @@
 
-// La bannière de maintenance pour les pages de connexion, d'inscription et de déconnexion
-
-@section('bannieremaintenance')
-
-    <div class="drapeaumaintenancepetit">
-        <h4>!</h4>
-    </div>
-
-    <div class="bannieremaintenance">
-        <h5>Maintenance</h5>
-        <p>Le site est actuellement en maintenance, vous pourriez rencontrer des problemes</p>
-    </div>
-
-@endsection
