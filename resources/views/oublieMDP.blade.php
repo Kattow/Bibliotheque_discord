ca93b0729363d61e1ad351fbcd817b4dc56d9440
Kattow <37405746+Kattow@users.noreply.github.com>
2018-07-12 09:27:21 +0200

    Mdr le français c'est pas évident
    "recut" ? Vraiment ? XD

--- resources/views/oublieMDP.blade.php
+++ resources/views/oublieMDP.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="oubliemdp-content">
 	<h5>Un mail permettant de reinitialiser votre mot de passe vous a été envoyé</h5>
-	<h5>Vous n'avez pas recut le mail ?</h5>
+	<h5>Vous n'avez pas reçu le mail ?</h5>
 	<button type="button" name="button" class="bouton-renvoyer">Renvoyer un mail</button>
 </div>
 
